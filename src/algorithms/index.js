cdc3091ae0f068cab64ffccbc8bd954b383af2a9
André Storhaug <andr3.storhaug@gmail.com>
2020-04-27 03:44:20 +0200

    Add algorithm module index file

---
+++ src/algorithms/index.js
@@ -0,0 +1,9 @@
+/**
+ * Voxelization algorithms.
+ * @module voxelizer/algorithms
+ */
+
+export { Algorithm } from "./Algorithm";
+export { ColorableAlgorithm } from "./ColorableAlgorithm";
+export { RaycastAlgorithm } from "./RaycastAlgorithm";
+export { AlgorithmFactory } from "./AlgorithmFactory";
